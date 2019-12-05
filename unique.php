52837783496d5ad1bf346f2d5654fc1c9124aba9
chris <chris.ayoubtexas@gmail.com>
2019-12-05 16:44:38 -0600

    Query change

--- unique.php
+++ unique.php
@@ -3,10 +3,16 @@
 include 'db.php';
 
 // Get a query for rare names by year and gender
+// Frequency of 5 is the absolute min
 function getQuery($year, $gender) {
     return
-        "SELECT name, frequency FROM names WHERE sex = '$gender' AND year = $year 
-                ORDER BY frequency ASC LIMIT 20";
+        "SELECT * FROM (
+                      SELECT name, SUM(frequency) AS f
+                      FROM names
+                      WHERE sex = '$gender'
+                        AND year = $year
+                      GROUP BY name
+                  ) AS n WHERE n.f = 5 ORDER BY RAND() LIMIT 20";
 }
 
 function printHeader() {
@@ -26,53 +32,40 @@ function printHeader() {
 ");
 }
 
-        printHeader();
-        global $db;
-        $data = [];
-        $doQuery = array_key_exists("year", $_GET);
-        if ($doQuery){
-            $year = $_GET['year'];
-            $gender = $_GET['gender'];
-            $genderName = '';
-            if ($gender == "M"){
-                $genderName = "Male";
-            }
-            else {
-                $genderName = "Female";
-            }
-            $query = getQuery($year, $gender);
-            $result = [];
-            $rows = mysqli_query($db, $query);
-            echo "<h3>The Most Unique $genderName Names in $year</h3>";
-            echo "<table><tr><th>Name</th><th>frequency</th><tr>";
-            while ($row = mysqli_fetch_array($rows)) {
-                echo "<tr><th>" . $row['name'] . "</th><th>" . $row['frequency'] . "</th></tr>";
-            }
+    printHeader();
+    global $db;
+    $data = [];
+    $doQuery = array_key_exists("year", $_GET);
+    if ($doQuery){
+        $year = $_GET['year'];
+        $gender = $_GET['gender'];
+        $genderName = '';
+        if ($gender == "M"){
+            $genderName = "Male";
         }
+        else {
+            $genderName = "Female";
+        }
+        $query = getQuery($year, $gender);
+        $result = [];
+        $rows = mysqli_query($db, $query);
+        echo "<h3>Some Unique $genderName Names in $year</h3>";
+        echo "<ul>";
+        while ($row = mysqli_fetch_array($rows)) {
+            echo "<li>" . $row['name'] . "</li>";
+        }
+        echo "</ul>";
+    }
 
     print "<h1> Choose a year: </h1>";
     print "<form method=GET action='unique.php' class='text-center'><select name='year'>";
-    for ($i = 2018; $i >= 2000; $i--) {
+    for ($i = YEAR_MAX; $i >= YEAR_MIN; $i--) {
         print "<option value='$i'> $i </option>";
     }
     print "</select>";
     print "<h1> Choose a gender: </h1>";
     print "<select name='gender'><option value='M'>Male</option><option value='F'>Female</option></select>";
     print "<input type='submit' name='submit' value='submit'></form><br>";
-
-
-
-        // while ($row = mysqli_fetch_array($rows)) {
-        //     $result = $row['name'];
-        // }
-        // foreach ($result as $key) {
-        // // print "<table>";
-        // print "$key";
-        // // print "<tr><td>$key<td><tr>";
-        // // print "</table>";
-        // }
-
-//Edited - added semicolon at the End of line.1st and 4th(prev) line
 
     // Print back button
     backButton();
